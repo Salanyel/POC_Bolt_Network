c8ed3ee50bf8da884c05e1815f03a566e658d5e0
Xyunn <maxime.devecchi@gmail.com>
2018-05-02 23:39:48 -0400

    Two scenes for server

---
+++ POC_Bolt/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : Bolt.GlobalEventListener
+{
+    private static NetworkManager _instance;
+
+    private static string _ipAddress = "127.0.0.1:27000";
+
+    public static NetworkManager Instance
+    {
+        get { if (_instance == null)
+            {
+                _instance = new NetworkManager();
+            }
+
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void CreateServer()
+    {
+        BoltLauncher.StartServer(UdpKit.UdpEndPoint.Parse(_ipAddress));
+    }
+
+    public void JoinServer()
+    {
+        BoltLauncher.StartClient();
+    }
+
+    IEnumerator TravelCamera()
+    {
+        Transform startPosition = Camera.main.gameObject.transform;
+        Transform destination = GameObject.FindGameObjectWithTag("EndPoint").transform;
+
+        float currentTime = 0f;
+
+        while (currentTime < 5f)
+        {
+            Camera.main.transform.position = Vector3.Lerp(startPosition.position, destination.position, currentTime / 5f);
+            Camera.main.transform.rotation = Quaternion.Lerp(startPosition.rotation, destination.rotation, currentTime / 5f);
+
+            yield return new WaitForEndOfFrame();
+            currentTime += Time.deltaTime;
+        }
+    }
+
+    #region Callbacks
+
+    public override void BoltStartDone()
+    {
+        Debug.Log("1");
+        if (BoltNetwork.isServer)
+        {
+            Debug.Log("2");
+            BoltNetwork.LoadScene("_MainScene");
+        } else
+        {
+            BoltNetwork.Connect(UdpKit.UdpEndPoint.Parse(_ipAddress));
+        }
+    }
+
+    public override void SceneLoadLocalDone(string map)
+    {
+        StartCoroutine(TravelCamera());
+    }
+
+    #endregion
+}
---
+++ POC_Bolt/Assets/Scripts/UILibrary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILibrary : MonoBehaviour {
+
+	public void OnClickToCreateSever()
+    {
+        NetworkManager.Instance.CreateServer();
+    }
+
+    public void OnClickToJoinSever()
+    {
+        NetworkManager.Instance.JoinServer();
+    }
+}
